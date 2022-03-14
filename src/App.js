4087b78b201546ddba77dc47cdc73a637216e8c5
bruno <bruno.cami@hotmail.com>
2022-03-14 20:14:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from './Home';
+import About from './About';
+import Service from './Service';
+
+function App() {
+  return (
+    <div className="App">
+        <Home/>
+        <About/>
+        <Service/>
+    </div>
+  );
+}
+
+export default App;
